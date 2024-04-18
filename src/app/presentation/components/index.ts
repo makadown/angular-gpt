2e7f679127c4605dc739b8d016c182b2b32324b1
makadown <maseflores79@gmail.com>
2024-04-18 11:28:49 -0700

    Agregando aliases de componentes

---
+++ src/app/presentation/components/index.ts
@@ -0,0 +1,3 @@
+export * from './chat-bubbles/chatMessage/chatMessage.component';
+export * from './chat-bubbles/myMessage/myMessage.component';
+export * from './sidebarMenuItem/sidebarMenuItem.component';
