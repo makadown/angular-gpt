1b33531a76a5ab78f8c161f356eb3e20a82580a6
makadown <maseflores79@gmail.com>
2024-04-18 12:06:55 -0700

    Caja de envio de mensajes

---
+++ src/app/presentation/components/text-boxes/textMessageBox/textMessageBox.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-message-box',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './textMessageBox.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextMessageBoxComponent { 
+  @Input() placeholder = '';
+  @Input() disableCorrectionsplaceholder = false;
+
+  @Output() onMessage = new EventEmitter<string>();
+
+  public fb = inject(FormBuilder);
+  public form = this.fb.group({
+    prompt: ['', Validators.required]
+  });
+
+  handleSubmit() {
+    if (this.form.valid) {      
+      const {prompt} = this.form.value;// esto es igual a const prompt = this.form.value.prompt;
+      console.log(prompt);
+      this.onMessage.emit(prompt??'');
+      this.form.reset();
+    }
+  }
+}
--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent, MyMessageComponent } from '@components/index';
+import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TypingLoaderComponent } from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
   imports: [
-    CommonModule,ChatMessageComponent,MyMessageComponent
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
