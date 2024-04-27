3aef17ec44a331e50f79d61e65f33ff6e0969683
makadown <maseflores79@gmail.com>
2024-04-27 14:10:48 -0700

    creando caso de uso de ortografia

---
+++ src/app/core/use-cases/orthography/orthography.use-case.ts
@@ -0,0 +1,31 @@
+import { OrthographyResponse } from "@interfaces/orthography.response";
+import { environment } from "environments/environment.development";
+
+export const orthographyUseCase = async(prompt:string) => {
+    try{
+        const response = await fetch(`${environment.backendApi}/orthography-check`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({prompt})
+        });
+
+        if(!response.ok){ throw new Error('No se pudo procesar la ortografía'); }
+
+        const data = await response.json() as OrthographyResponse;
+        return {
+            ok: true,
+            ...data
+        }
+    }
+    catch(e){
+        console.log(e);
+        return {
+            ok: false,
+            userScore: 0,
+            errors: [],
+            message: 'No se pudo procesar la ortografía'
+        }
+    }
+}
